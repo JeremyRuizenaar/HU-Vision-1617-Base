b3163c8656acd359b50a7ca530b07910fa8dc3c1
Jeremy Ruizenaar <jeremy.ruizenaar@student.hu.nl>
2017-04-14 05:58:29 +0200

    prakticum vision finished

--- source/ExternalDLL/ExternalDLL/StudentLocalization.cpp
+++ source/ExternalDLL/ExternalDLL/StudentLocalization.cpp
@@ -1,6 +1,13 @@
 #include "StudentLocalization.h"
 
 bool StudentLocalization::stepFindHead(const IntensityImage &image, FeatureMap &features) const {
+	//zoek op de x as beginnend vanaf links naar de eerste kolom met pixels 
+	// zoek op de x as beginnen vanaf rechts naar de eerste kolom met pixels
+	//zoek op de y as beginnen vanaf  boven naar de eerst rij met pixels
+	// sla links en rechts op als coordinaat 
+	// bereken  de x waarde van het hoofd door het x-coordinaat tussen hoofdlinks en hoofd rechts te nemen
+	//ken deze x waarde toe aan het hoofdbovenkant coordinaat 
+	//sla de 3 waardes op
 	return false;
 }
 
